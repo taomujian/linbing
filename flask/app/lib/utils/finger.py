8ec8508cecea6f13d9b5b3ce0ddd7b731691c619
taomujian <taomujian@protonmail.com>
2021-05-27 16:04:56 +0800

    v2.3

--- flask/app/lib/utils/finger.py
+++ flask/app/lib/utils/finger.py
@@ -3,7 +3,6 @@
 import re
 import sys
 import time
-import random
 import asyncio
 import hashlib
 import functools
@@ -194,48 +193,7 @@ class WhatCms:
         m.update(content)
         hash_str = m.hexdigest()
         return hash_str
-    
-    def get_useragent():
 
-        """
-        随机生成一个请求头
-
-        :param:
-
-        :return dict headers: 生成的请求头
-        """
-
-        user_agent = [
-            'Mozilla/5.0 (Windows; U; Win98; en-US; rv:1.8.1) Gecko/20061010 Firefox/2.0',
-            'Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US) AppleWebKit/532.0 (KHTML, like Gecko) Chrome/3.0.195.6 Safari/532.0',
-            'Mozilla/5.0 (Windows; U; Windows NT 5.1 ; x64; en-US; rv:1.9.1b2pre) Gecko/20081026 Firefox/3.1b2pre',
-            'Opera/10.60 (Windows NT 5.1; U; zh-cn) Presto/2.6.30 Version/10.60','Opera/8.01 (J2ME/MIDP; Opera Mini/2.0.4062; en; U; ssr)',
-            'Mozilla/5.0 (Windows; U; Windows NT 5.1; ; rv:1.9.0.14) Gecko/2009082707 Firefox/3.0.14',
-            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36',
-            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
-            'Mozilla/5.0 (Windows; U; Windows NT 6.0; fr; rv:1.9.2.4) Gecko/20100523 Firefox/3.6.4 ( .NET CLR 3.5.30729)',
-            'Mozilla/5.0 (Windows; U; Windows NT 6.0; fr-FR) AppleWebKit/528.16 (KHTML, like Gecko) Version/4.0 Safari/528.16',
-            'Mozilla/5.0 (Windows; U; Windows NT 6.0; fr-FR) AppleWebKit/533.18.1 (KHTML, like Gecko) Version/5.0.2 Safari/533.18.5'
-        ]
-        user_agent = random.choice(user_agent)
-        return user_agent
-    
-    def requests_headers(self):
-
-        """
-        随机生成一个请求头
-
-        :param:
-
-        :return dict headers: 生成的请求头
-        """
-
-        UA = self.get_useragent()
-        headers = {
-            'User-Agent': UA
-        }
-        return headers
-    
     def request_url(self, url):
 
         """
@@ -247,7 +205,11 @@ class WhatCms:
         """
 
         try:
-            req = request.get(url = url, headers = self.requests_headers(), allow_redirects = False)
+            UA = get_useragent()
+            headers = {
+                'User-Agent': UA
+            }
+            req = request.get(url = url, headers = headers, allow_redirects = False)
             self.num = self.num + 1
             req.encoding = 'utf-8'
             if req.status_code == 200:
@@ -286,6 +248,7 @@ class WhatCms:
                         task.cancel() 
                     loop.stop()
                     loop.close()
+
                 if response_html and response_content:
                     if options == 'md5':
                         if match_pattern == self.getMD5(response_content):
@@ -336,7 +299,6 @@ class WhatCms:
                                 task.cancel() 
                             loop.stop()
                             loop.close()
-
             except Exception as e:
                 # print(e)
                 pass
